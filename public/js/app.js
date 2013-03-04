0987aad2a2aed4d736a258a096204fd069e6276d
David Chawei Hsu <chawei.hsu@gmail.com>
2013-03-04 03:04:20 -0800

    add bookmark feature

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,9 @@ var App = function() {
   };
   
   //this.currentUser = null;
+  //var checkinsRef = new Firebase("https://steampunk.firebaseIO.com/users/685681169/checkins");
+  //var checkinsQuery = checkinsRef.startAt(10151347716696170);
+  //checkinsQuery.on('child_added', function(childSnapshot) { /* handle child add */ });
 };
 
 App.prototype = {
@@ -16,27 +19,106 @@ App.prototype = {
     this.Config.currentUserRef.on('value', function(snapshot) {
       currentUser = snapshot.val();
       that.renderUI(currentUserId);
+      that.renderBookmarks(currentUser.bookmarks);
       that.renderMap();
     });
+    
+    this.Config.bookmarksRef = new Firebase(App.Config.FirebaseURL + '/users' + '/' + currentUserId + '/bookmarks');
   },
   
   renderUI : function(currentUserId) {
     $('#map-ui').removeClass('hidden');
     $('#me_btn').attr('user-id', currentUserId);
+    
+    var that = this;
+    $(document).on("click", '.marker-title .save_btn', function(){
+      var markerTooltip = $(this).parents('.marker-tooltip');
+      var markerTitle   = markerTooltip.find('.marker-title');
+      var checkinId     = markerTitle.attr('checkin-id');
+      console.log('Save '+markerTitle.text());
+      console.log(checkinId);
+      
+      var checkin = that.findCheckin(checkinId);
+      if (checkin !== null) {
+        var checkinId = parseFloat(checkinId);
+        var bookmarksRef = new Firebase("https://steampunk.firebaseIO.com/users/"+currentUserId+"/bookmarks");
+        var bookmarksQuery = bookmarksRef.startAt(checkinId).endAt(checkinId);
+        bookmarksQuery.on('value', function(snapshot) {
+          if (snapshot.val() === null) {
+            var newBookmarkRef = App.Config.bookmarksRef.push();
+            newBookmarkRef.setWithPriority(checkin, checkinId);
+          }
+        });
+      }
+      return false;
+    });
+  },
+  
+  removeBookmark : function(bookmarkId) {
+    var currentUserId = currentUser.fb_info.id;
+    var bookmarkRef   = new Firebase("https://steampunk.firebaseIO.com/users/"+currentUserId+"/bookmarks/"+bookmarkId);
+    bookmarkRef.remove();
+  },
+  
+  fetchAndRenderBookmarks : function(userId) {
+    var that = this;
+    var bookmarksRef = new Firebase("https://steampunk.firebaseIO.com/users/"+userId+"/bookmarks");
+    var bookmarksQuery = bookmarksRef.limit(10);
+    bookmarksQuery.on('value', function(snapshot) {
+      var bookmarks = snapshot.val();
+      that.renderBookmarks(bookmarks);
+    });
+  },
+  
+  renderBookmarks : function(bookmarks) {
+    var bookmarksElem = $('#bookmarks');
+    bookmarksElem.empty();
+    for (bookmarkId in bookmarks) {
+      var bookmark = bookmarks[bookmarkId];
+      bookmarksElem.prepend('<div class="bookmark" bookmark-id="'+bookmarkId+'">'+bookmark.place.name+'<a class="remove_btn" href="#">remove</a></div>');
+    }
+    
+    var that = this;
+    $('.remove_btn').click(function(e){
+      e.preventDefault();
+      
+      var btn          = $(this);
+      var bookmarkElem = btn.parents('.bookmark');
+      var bookmarkId   = bookmarkElem.attr('bookmark-id');
+      that.removeBookmark(bookmarkId);
+      bookmarkElem.remove();
+    });
+  },
+  
+  findCheckin : function(checkinId) {
+    var targetCheckin = null;
+    $.each(currentUser.checkins, function(index, checkin) {
+      if (checkin.id === checkinId.toString()) {
+        targetCheckin = checkin;
+        return false;
+      }
+    });
+    return targetCheckin;
   },
   
   renderMap : function() {
     var map = mapbox.map('map');
     map.addLayer(mapbox.layer().id('chawei.map-85tzbb2w'));
-
-    // Create an empty markers layer
+    
     var markerLayer = mapbox.markers.layer();
-
-    // Add interaction to this marker layer. This
-    // binds tooltips to each marker that has title
-    // and description defined.
-    mapbox.markers.interaction(markerLayer);
+    var interaction = mapbox.markers.interaction(markerLayer);
     map.addLayer(markerLayer);
+    
+    interaction.formatter(function(feature) {
+      var o = ['<div class="marker-title" checkin-id="', feature.properties["checkin-id"], '">',
+                  feature.properties.title,
+                  '<a class="save_btn" href="#">save</a>',
+                '</div>',
+                '<div class="marker-description">',
+                  feature.properties.description,
+                '</div>'].join('');
+      return o;
+    });
 
     map.zoom(10).center({ lat: 37.626, lon: -122.397 });
     
@@ -44,7 +126,9 @@ App.prototype = {
     //this.importCheckIns(markerLayer, CHECK_INS['jackie'].checkins.data, CHECK_INS['jackie'].checkins.id);
     //this.importCheckIns(markerLayer, CHECK_INS['wei'].checkins.data, CHECK_INS['wei'].checkins.id);
 
-    $('.filter').click(function(){
+    $('.filter').click(function(e){
+      e.preventDefault();
+      
       var thisElem = $(this);
       var userId   = thisElem.attr('user-id');
       if (thisElem.hasClass('active')) {
@@ -59,8 +143,7 @@ App.prototype = {
       });
       markerLayer.filter(function(f) {
         return ($.inArray(f.properties['user-id'], activeUserIds) !== -1);
-      }); 
-      return false;
+      });
     });
 
     markerLayer.factory(function(f) {
@@ -94,6 +177,7 @@ App.prototype = {
           //'marker-symbol': 'star-stroked',
           'user-id': userId,
           'image': 'https://graph.facebook.com/'+userId+'/picture',
+          'checkin-id': checkin.id,
           title: checkin.place.name,
           description: description
         }
